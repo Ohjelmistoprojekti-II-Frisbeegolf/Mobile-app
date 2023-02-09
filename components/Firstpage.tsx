37b850ff1e92d9be4cff760e32f2afc0c008bd1a
jaakko0354 <koobixp1@gmail.com>
2023-02-09 13:03:45 +0200

    updated react-native version

---
+++ components/Firstpage.tsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react';
+import { NativeBaseProvider,Box } from 'native-base';
+import {Text,View,Button} from 'native-base';
+
+export default function Firstpage(){
+    return(
+        <View>
+            <Text>Firstpage</Text>
+        </View>
+    );
+}
