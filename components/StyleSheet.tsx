15fc2244dfeb77c7c5f5c65c84a24a262f3101fe
Jani <jani.n@icloud.com>
2023-02-13 14:52:58 +0200

    Added stylesheet

---
+++ components/StyleSheet.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+
+    const styles = StyleSheet.create({
+        
+        text: {
+            color: 'green',
+            fontSize:22,
+        }
+    });
+
+    export { styles }
