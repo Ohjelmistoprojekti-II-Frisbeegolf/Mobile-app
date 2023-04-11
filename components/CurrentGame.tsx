8a0f147ebd0ae28be17c586229844b84cc8e477f
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-04-11 17:02:42 +0300

    First version

--- components/CurrentGame.tsx
+++ components/CurrentGame.tsx
@@ -1,82 +1,104 @@
-import {Text,View,Button} from 'native-base';
+import { Text, View, Button } from 'native-base';
 import React, { useState, useEffect } from 'react';
 import { styles } from './StyleSheet';
-import { ActivityIndicator } from 'react-native'
+import { ActivityIndicator, GestureResponderEvent } from 'react-native'
+import Carousel from 'react-native-reanimated-carousel';
 
 interface Course {
     courseId: number,
     courseName: string,
-    holes: {
-        holeId: number,
-        holeNumber: number,
-        holePar: number,
-    }
+    holes: Hole[]
 }
 
-const ThrowCounter = () => {
-    const [throwCount, setThrowCount] = useState(0);
-    if (throwCount < 0 ) {
-        setThrowCount(0)
-    }
-    return (
-        <View style={styles.throwCounterView}>
-            <Button
-                _pressed={{ opacity: 0.5 }}
-                style={styles.throwButton}
-                onPress={() => setThrowCount(throwCount - 1)}>
-            -
-            </Button>
-            <Text style={styles.throwCounterText}>{throwCount}</Text>
-            <Button 
-                _pressed={{ opacity: 0.5 }}
-                style={styles.throwButton}
-                onPress={() => setThrowCount(throwCount + 1)}>
-            +
-            </Button>
-        </View>
-    )
-
+interface Hole {
+    holeId: number,
+    holeLength: number,
+    holeNumber: number,
+    holePar: number
 }
 
-export default function CurrentGame({navigation}:  {navigation: any}) {
+interface Stroke {
+    hole: Hole
+    gameId: number
+    courseId: number
+    score: number
+}
+
+export default function CurrentGame({ navigation }: { navigation: any }) {
 
     const [repository, setRepository] = useState<Course>({
         courseId: 1,
         courseName: "",
-        holes: {
-            holeId: 1,
-            holeNumber: 1,
-            holePar: 1,
-            }
+        holes: []
     });
-    
+    const [strokes, setStrokes] = useState<Stroke[]>([]);
+    const [index, setIndex] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    useEffect(() => fetchData(),[]);
+
+    useEffect(() => fetchData(), []);
+
+    useEffect(() => {
+        setStrokes(repository.holes.map(hole => { return ({ hole: hole, gameId: 4, courseId: 1, score: 0 }) }))
+    }, [repository]);
+
+    const handleStroke = (index: number, operator: string) => {
+        if (operator === '+') {
+            const data = [...strokes];
+            data[index].score = data[index].score + 1;
+            setStrokes(data);
+        } else {
+            const data = [...strokes];
+            data[index].score = data[index].score - 1;
+            setStrokes(data);
+        }
+        console.log(strokes)
+    }
+
 
     const fetchData = () => {
         fetch('https://dev-discgolf.herokuapp.com/courses/1')
-        .then(res => res.json())
-        .then(data => {
-            setRepository(data)
-        }
-        )
-        .catch(err => console.log(err))
-        console.log(repository.holes)
+            .then(res => res.json())
+            .then(data => {
+                setRepository(data)
+            }
+            )
+            .catch(err => console.log(err))
     }
 
-    return(
-        <View style={styles.view}>
-            <Text style={styles.header}>Väylä: {repository.holeNumber}</Text>
-            <Text style={styles.header}>Par: {repository.holePar}</Text>
-            <Text style={styles.header}> Heitot: </Text>
-        <View style={{marginBottom: '120%'}}>
-            <ThrowCounter />
-        </View>
-        <View style={styles.throwButtonView}>
-                <Button _pressed={{ opacity: 0.5 }} style={styles.nextPreviousButton}>Edellinen väylä (#)</Button>
-                <Button _pressed={{ opacity: 0.5 }} style={styles.nextPreviousButton}>Seuraava väylä (#)</Button>
-        </View>
-        </View>
-    );
+    return (
+        strokes.length < 1 ?
+            <View></View>
+            :
+            <View style={styles.view}>
+                <View style={styles.throwButtonView}>
+                    <Button _pressed={{ opacity: 0.5 }} style={styles.nextPreviousButton} onPress={() => setIndex(index - 1)} >Edellinen väylä (#)</Button>
+                    <Button _pressed={{ opacity: 0.5 }} style={styles.nextPreviousButton} onPress={() => setIndex(index + 1)} >Seuraava väylä (#)</Button>
+                </View>
+                <Text style={styles.header}>Väylä: {strokes[index].hole.holeNumber}</Text>
+                <Text style={styles.header}>Par: {strokes[index].hole.holePar}</Text>
+                <Text style={styles.header}>Pituus: {strokes[index].hole.holeLength}m</Text>
+                <Text style={styles.header}> Heitot: </Text>
+                <View style={{ marginBottom: '120%' }}>
+                    <View style={styles.throwCounterView}>
+                        <Button
+                            _pressed={{ opacity: 0.5 }}
+                            style={styles.throwButton}
+                            onPress={() => handleStroke(index, '-')}
+                        >
+                            -
+                        </Button>
+                        <Text style={styles.throwCounterText}>{strokes[index].score}</Text>
+                        <Button
+                            _pressed={{ opacity: 0.5 }}
+                            style={styles.throwButton}
+                            onPress={() => handleStroke(index, '+')}
+                        >
+                            +
+                        </Button>
+                    </View>
+                </View>
 
+            </View>
+    );
 }
