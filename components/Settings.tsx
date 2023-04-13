d7bbd8268ec9110750147129100b254aacb72b5a
jaakko0354 <koobixp1@gmail.com>
2023-04-13 12:34:54 +0300

    Settings-page logout function

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -2,15 +2,19 @@ import MyDrawer from "./Menu";
 import {Text,View,Button,Image} from 'native-base';
 import React from 'react';
 import { styles } from './StyleSheet'
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function Settings() {
-
+export default function Settings(props:any) {
+    const kirjauduUlos = async () => {
+        await AsyncStorage.removeItem('token');
+        props.setLoggedIn(false);
+    }
     return(
         <View style={styles.view}>
             <Button _pressed={{ opacity: 0.5 }} style={styles.button}>Nollaa tilastot</Button>
             <Button _pressed={{ opacity: 0.5 }} style={styles.button}>Puhelimen asetukset</Button>
             <Button _pressed={{ opacity: 0.5 }} style={styles.button}>Poista tunnus</Button>
-            <Button _pressed={{ opacity: 0.5 }} style={styles.button}>Kirjaudu ulos</Button>
+            <Button _pressed={{ opacity: 0.5 }} style={styles.button} onPress={kirjauduUlos}>Kirjaudu ulos</Button>
         </View>
     );
 
